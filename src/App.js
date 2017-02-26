fc32d195b3f077b80fbb7f357c57eff423dff3f4
jason <changyuanjian@gmail.com>
2017-02-26 12:36:43 +0800

    feat: channelSection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, { Component } from 'react';
+
+import ChannelSection from './commponents/channels/ChannelSection';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      channels: [],
+      activeChannel:{}
+    }
+  }
+
+  addChannel(name) {
+    let {channels} = this.state;
+    channels.push({ id: channels.length, name })
+    this.setState({ channels });
+    //TODO send to server
+  }
+  setChannel(activeChannel) {
+    this.setState({ activeChannel })
+    //TODO Get Channnels Messages
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="nav">
+          <ChannelSection
+            {...this.state}
+            addChannel={this.addChannel.bind(this)}
+            setChannel={this.setChannel.bind(this)}
+          />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/commponents/channels/ChannelList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import Channel from './Channel';
+
+class ChannelList extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.channels.map(chan =>
+          <Channel
+            channel={chan}
+            key={chan.id}
+            {...this.props}
+          />
+        )}
+      </ul>
+    )
+  }
+}
+
+ChannelList.propTypes = {
+  channels: React.PropTypes.array.isRequired,
+  setChannel: React.PropTypes.func.isRequired,
+  activeChannel: React.PropTypes.object.isRequired
+}
+
+export default ChannelList;
